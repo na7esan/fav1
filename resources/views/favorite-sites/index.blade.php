2e29a83d6df64c2aa663238d18a400f61c0e3c4f
na7esan <na7esan@gmail.com>
2021-08-06 18:52:59 +0900

    add index view

--- resources/views/favorite-sites/index.blade.php
+++ resources/views/favorite-sites/index.blade.php
@@ -1,3 +1,14 @@
-@php
-    ddd($favoriteSites);
-@endphp
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>お気に入りサイト一覧ページ</title>
+</head>
+<body>
+    <h1>お気に入りサイト一覧</h1>
+    @foreach ($favoriteSites as $favoriteSite)
+        <p>{{ $favoriteSite->title }}</p>
+        <p>{{ $favoriteSite->url }}</p>
+    @endforeach
+</body>
+</html>
