8fd50c58501d63f6f92b3b7effa5b9e8bc3d594e
na7esan <na7esan@gmail.com>
2021-08-06 17:18:55 +0900

    add create route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FavoriteSiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('favorite-sites/create', [FavoriteSiteController::class, 'create']);
